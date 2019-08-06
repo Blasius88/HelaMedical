a19625927fb24b5511c68795127223680709e442
Roman Dolbik-Vorobey <dolbik-w.by@mail.ru>
2019-08-06 17:10:10 +0300

    Добавьте файлы проекта.

---
+++ HelaMedical/Interface.xaml.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Windows;
+using HelaMedical.Class;
+using HelaMedical.Excep;
+
+namespace HelaMedical
+{
+    /// <summary>
+    /// Логика взаимодействия для Interface.xaml
+    /// </summary>
+    public partial class Interface : Window
+    {
+        ApplicationContext db;
+        public List<Alco> alcoFindPerson = new List<Alco>();
+        public List<Narcoman> narcoFindPerson = new List<Narcoman>();
+        public List<Polizavis> polizFindPerson = new List<Polizavis>();
+
+        public Interface()
+        {
+            db = new ApplicationContext();
+            InitializeComponent();
+            this.Closing += Generate_A_Report_Closing;
+        }
+
+        private void Open_OblReg()
+        {
+            MessageBox.Show("Не выбрали область и регион");
+            OblReg oblReg = new OblReg();
+            oblReg.Show();
+            return;
+        }
+        private void Generate_A_Report_Closing(object sender, CancelEventArgs e)
+        {
+            try
+            {
+                if (db != null) db.Dispose();
+                DbContexte.DBConection.Closed();
+            }
+            catch (Exception excep)
+            {
+                MessageBox.Show(excep.Message);
+                ExcepLog.Excep(excep);
+            }
+        }
+
+        /// <summary>
+        /// открыть окно с алкоголиками 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Add_Order(object sender, RoutedEventArgs e)
+        {
+            if (Other.Obl == null || Other.Reg == null)
+            {
+                Open_OblReg();
+            }
+            else
+            {
+                MainDataWindow mainDataWindow = new MainDataWindow();
+                mainDataWindow.Show();
+            }
+        }
+
+        /// <summary>
+        /// открыть окно с наркоманами 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Edit_Order(object sender, RoutedEventArgs e)
+        {
+            if (Other.Obl == null || Other.Reg == null)
+            {
+                Open_OblReg();
+            }
+            else
+            {
+                MainDataWindow2 mainDataWindow2 = new MainDataWindow2();
+                mainDataWindow2.Show();
+            }
+        }
+
+        /// <summary>
+        /// открыть окно с полизависимыми 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Del_Order(object sender, RoutedEventArgs e)
+        {
+            if (Other.Obl == null || Other.Reg == null)
+            {
+                Open_OblReg();
+            }
+            else
+            {
+                MainDataWindow3 mainDataWindow3 = new MainDataWindow3();
+                mainDataWindow3.Show();
+            }
+        }
+
+        /// <summary>
+        /// Создать отчет по определенным критериям 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Generate_A_Report(object sender, RoutedEventArgs e)
+        {
+            Generate_A_Report generate_A_Report = new Generate_A_Report();
+            generate_A_Report.Show();
+        }
+
+        /// <summary>
+        /// Выбрать Область и регион
+        /// </summary>
+        private void MenuItem_OblReg_Click(object sender, RoutedEventArgs e)
+        {
+            OblReg oblReg = new OblReg();
+            oblReg.Show();
+        }
+
+        /// <summary>
+        /// Поиск карты пациента 
+        /// </summary>
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            bool check_alco = false;
+            bool check_narco = false;
+            bool check_poliz = false;
+            string findperson = FindPerson.Text;
+
+            //Очищаем память перед записью новой информации 
+            alcoFindPerson.Clear();
+            narcoFindPerson.Clear();
+            polizFindPerson.Clear();
+
+            //Ищим инфу в колекциях
+            for (int i = 0; i < Alco.AlcoholismPersona.Count; i++)
+            {
+                if (findperson == Alco.AlcoholismPersona[i].FIO)
+                {
+                    check_alco = true;
+                    alcoFindPerson.Add(Alco.AlcoholismPersona[i]);
+                }
+            }
+            
+            for (int i = 0; i < Narcoman.Drug_Addiction.Count; i ++)
+            {
+                if (findperson == Narcoman.Drug_Addiction[i].FIO)
+                {
+                    check_narco = true;
+                    narcoFindPerson.Add(Narcoman.Drug_Addiction[i]);
+                }
+            }
+            
+            for (int i = 0; i < Polizavis.Alco_Narco_Person.Count; i++)
+            {
+                if (findperson == Polizavis.Alco_Narco_Person[i].FIO)
+                {
+                    check_poliz = true;
+                    polizFindPerson.Add(Polizavis.Alco_Narco_Person[i]);
+                }
+            }
+
+            if (check_alco == true)
+            {
+                //Выводим инфу на экран 
+                ListOrder.ItemsSource = alcoFindPerson;
+            }
+            else if (check_narco == true)
+            {
+                ListOrder.ItemsSource = narcoFindPerson;
+
+            }
+            else if (check_poliz == true)
+            {
+                ListOrder.ItemsSource = polizFindPerson;
+            }
+            else if (check_alco == false && check_narco == false && check_poliz == false)
+            {
+                MessageBox.Show("ФИО не найдено");
+            }
+        }
+    }
+}
---
+++ HelaMedical/MainWindow.xaml.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Data.Entity;
+using System.Windows;
+using HelaMedical.Class;
+using HelaMedical.Excep;
+using HelaMedical.DbContexte;
+using System.IO;
+using System.Data.SqlClient;
+
+namespace HelaMedical
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        // BackgroundWorker backgroundWorker;
+
+        ApplicationContext db;
+
+        public MainWindow()
+        {
+            try
+            {
+                FileLoad();
+                db = new ApplicationContext();
+                db.Incitalizations.Load(); //загружаем из бд именна 
+                InitializeComponent();
+            }
+            catch (Exception excep)
+            {
+                MessageBox.Show(excep.Message);
+                ExcepLog.Excep(excep);
+            }
+        }
+        /// <summary>
+        /// Регистрация нового сотрудника 
+        /// </summary>
+        public void Button_Registration(object sender, RoutedEventArgs e)
+        {
+            NewPersonalWindow newPersonal = new NewPersonalWindow();
+            newPersonal.Show();
+        }
+
+        public static void FileLoad()
+        {
+            try
+            {
+                string path = @"\HelaMedical\HelaMedical\File\INC.INI";
+                string s = File.ReadAllText(path);
+                DBConection.Select(s);
+            }
+            catch (Exception excep)
+            {
+                MessageBox.Show(excep.Message);
+                ExcepLog.Excep(excep);
+            }
+        }
+
+        /// <summary>
+        /// Вход в систему
+        /// </summary>
+        private void Button_Entry(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                bool c = false; // Для проверики наличия имени в массиве 
+                string a = tb.Text;
+                string b = Convert.ToString(pass.Password);
+                // проверяем соответствие имени и пароля в системе 
+                foreach (var sIncitalization in db.Incitalizations)
+                {
+                    if (a == sIncitalization.Name && b == sIncitalization.Pass)
+                    {
+                        c = true;
+                        Incitalization reg = new Incitalization
+                        {
+
+                            Name = a
+                        };
+                        Incitalization.Registr.Add(reg);
+                        Other.Regis = a; // запоминаем имя регистратора данных
+                        break;
+                    }
+                    // имя не найдено 
+                    else
+                    {
+                        c = false;
+                    }
+                }
+                
+                // если в массиве нет имени 
+                if (c == false)
+                {
+                    MessageBox.Show("Неправельно введен логин или пароль");
+                    tb.Clear(); // очищает поле логин
+                    pass.Clear(); // очищает поле пароль
+                }
+
+                else
+                {
+                    Interface iInterface = new Interface();
+                    iInterface.Show();
+                    Close();
+                }
+            }
+            catch (Exception excep)
+            {
+                MessageBox.Show(excep.Message);
+                ExcepLog.Excep(excep);
+                return;
+            }
+        }
+    }
+}
